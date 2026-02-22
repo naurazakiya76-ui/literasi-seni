453c11776debc8526b85e341480c3a6ae86a4f1c
naurazakiya76-ui <naurazakiya76@gmail.com>
2026-02-23 05:04:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,26 +21,35 @@ answer:2
 }
 ];
 
-let currentIndex = 0;
 let score = 0;
+let timeLeft = 30;
+let timerInterval;
+let currentQuestion;
 
+// ================= START QUIZ =================
 function startQuiz(){
+
 document.getElementById("startScreen").classList.remove("active");
 document.getElementById("quizScreen").classList.add("active");
+document.querySelector(".timer-wrapper").classList.add("active");
+
 showQuestion();
+startTimer();
 }
 
+// ================= SHOW QUESTION =================
 function showQuestion(){
 
-const q = questions[currentIndex];
+// ambil soal random
+currentQuestion = questions[Math.floor(Math.random()*questions.length)];
 
-document.getElementById("passage").textContent = q.passage;
-document.getElementById("question").textContent = q.question;
+document.getElementById("passage").textContent = currentQuestion.passage;
+document.getElementById("question").textContent = currentQuestion.question;
 
 const optionsDiv = document.getElementById("options");
 optionsDiv.innerHTML="";
 
-q.options.forEach(function(option,index){
+currentQuestion.options.forEach(function(option,index){
 const btn = document.createElement("button");
 btn.textContent = option;
 btn.onclick = function(){
@@ -48,36 +57,54 @@ selectAnswer(index);
 };
 optionsDiv.appendChild(btn);
 });
-
-document.getElementById("progress").textContent =
-"Soal " + (currentIndex+1) + " dari " + questions.length;
 }
 
+// ================= SELECT ANSWER =================
 function selectAnswer(index){
 
-if(index === questions[currentIndex].answer){
+if(index === currentQuestion.answer){
 score++;
 }
 
-currentIndex++;
-
-// kalau sudah sampai soal terakhir → ulang ke awal
-if(currentIndex >= questions.length){
-currentIndex = 0;
+showQuestion(); // langsung tampil soal baru
 }
 
-showQuestion();
+// ================= TIMER =================
+function startTimer(){
+
+const circle = document.getElementById("progressCircle");
+const total = 30;
+const circumference = 283;
+
+timerInterval = setInterval(function(){
+
+timeLeft--;
+document.getElementById("timerText").textContent = timeLeft;
+
+let offset = circumference - (timeLeft/total)*circumference;
+circle.style.strokeDashoffset = offset;
+
+if(timeLeft <= 0){
+finishQuiz();
 }
 
+},1000);
+}
+
+// ================= FINISH =================
 function finishQuiz(){
 
+clearInterval(timerInterval);
+
 document.getElementById("quizScreen").classList.remove("active");
+document.querySelector(".timer-wrapper").classList.remove("active");
 document.getElementById("resultScreen").classList.add("active");
 
 document.getElementById("finalScore").textContent =
-"Skor Kamu: " + score + " / " + questions.length;
+"Skor Kamu: " + score;
 }
 
+// ================= RESTART =================
 function restartQuiz(){
 location.reload();
 }
